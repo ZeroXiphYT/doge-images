c12c57a6715032bf54897c07cb9993d0d318997b
ZeroXiphYT <vachanbharucha11@gmail.com>
2022-08-30 18:30:16 -0700

    added code

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const _ = require('lodash');
+
+const app = express();
+app.options('/image', cors(corsOptions));
+
+
+var corsOptions = {
+    origin: "*"
+};
+
+app.get("/image", cors(corsOptions), (req, res) => {
+    const images = ["../images/doge1.jpeg", "../images/doge2.jpeg", "../images/doge3.jpeg"];
+
+
+    res.json({
+        imageUrl: _.sample(images)
+    });
+});
+
+app.listen(3000);
